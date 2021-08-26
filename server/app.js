1d7e3eef5e7803d16a87f4f5010b67c036cb2105
Jeffery Patterson <jpatterson933@ucla.edu>
2021-08-26 11:59:12 -0700

    added graphql middleware, next need to create and add schema

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+//must destructure graphqlhttp due to an update
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
+
+//this is a middleware function
+app.use('/graphql', graphqlHTTP({
+
+}));
 
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');
