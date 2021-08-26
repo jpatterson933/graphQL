87b960260058e84beed29c8efe4b086d61ca7fba
Jeffery Patterson <jpatterson933@ucla.edu>
2021-08-26 16:33:18 -0700

    added end of day comments

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,10 @@
+//here we are requiring graphql into the program which it will later be used to destructure schema elements
 const graphql = require('graphql');
 
+//here we are destructering schema typs from graphql itself
 const { GraphQLObjectType, GraphQLString } = graphql;
 
+//this is our graphql schema for books
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
