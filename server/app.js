0f7b9460758e120a2e438a87f7f6b63ef47cc0be
Jeffery Patterson <jpatterson933@ucla.edu>
2021-08-27 12:38:05 -0700

    added root query for book and exported to app.js and put schema into middleware

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
+//here we require express
 const express = require('express');
 //must destructure graphqlhttp due to an update
 const { graphqlHTTP } = require('express-graphql');
 
+const schema = require('./schema/schema');
+
+//here we define our express function as app
 const app = express();
 
 //this is a middleware function
 app.use('/graphql', graphqlHTTP({
+    schema: schema
 
 }));
 
+//this has our app listening and tells us that the server is live
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');
 })
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,7 +2,7 @@
 const graphql = require('graphql');
 
 //here we are destructering schema typs from graphql itself
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 //this is our graphql schema for books
 const BookType = new GraphQLObjectType({
@@ -13,3 +13,28 @@ const BookType = new GraphQLObjectType({
         genre: { type: GraphQLString }
     })
 });
+
+//the rootquery is how we initally jump into the graph
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    //each field will be a type of root query eg. grab a book, grab an author, grab all books or all authors etc.
+    fields: {
+        //when someone queryies a book, the code below will be used - using args 'id' GraphQL String
+        //then once the query is received, the resolve funciton will fire - the resolve function will take a parent parameter and an 
+        //args param that will ocntain an id feild the user sends along with the query
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            //parent comes into play when we look at relationships between data
+            resolve (parent, args) {
+                //code to get data from db / other source
+            }
+        }
+    }
+})
+
+//we create a new schema down here
+//pass options in and define the query as the root query we have defined above
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
