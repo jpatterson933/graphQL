7169efc1218d751a60db1059427d72b5d64d6504
Jeffery Patterson <jpatterson933@ucla.edu>
2021-08-29 14:47:01 -0700

    Mapping through the data that is now being grabbed from the backend, the name of each book is now displayed on the page

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -12,12 +12,23 @@ const getBooksQuery = gql`
 `
 
 class BookList extends Component {
+    displayBooks() {
+        var data = this.props.data;
+        if (data.loading) {
+            return (<div>Loading Books...</div>)
+        } else {
+            return data.books.map(book => {
+                return (
+                    <li key={book.id}>{book.name}</li>
+                );
+            })
+        }
+    }
     render() {
-        console.log(this.props)
         return (
             <div>
                 <ul id="book-list">
-                    <li>Harry Potter</li>
+                    {this.displayBooks()}
                 </ul>
             </div>
         )
