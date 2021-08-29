99d944b7d39ef25606e7d0879b7d1b34df343708
Jeffery Patterson <jpatterson933@ucla.edu>
2021-08-29 08:52:17 -0700

    Created first component BookList and it now renders on our app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 
+//components
+import BookList from "./components/BookList";
 
 function App() {
   return (
     <div id="main">
       <h1>Ninja's Reading List</h1>
+      <BookList />
     </div>
   );
 }
